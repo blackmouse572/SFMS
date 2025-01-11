af16fb2c5be957f8b43f8e2dbd85e3eea8433d48
blackmouse572 <ngocvlqt1995@gmail.com>
2025-01-11 13:39:48 +0700

    feat: simplify Vite configuration by removing MDX plugin and adjusting React plugin settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
-import mdx from '@mdx-js/rollup';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    {
-      enforce: 'pre',
-      ...mdx({
-        providerImportSource: '@mdx-js/react',
-      }),
-    },
-    react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/, exclude: /node_modules/ }),
-  ],
+  plugins: [react({ include: /\.(tsx|ts)$/, exclude: /node_modules/ })],
   resolve: {
     alias: {
       '@tailus-ui': path.resolve(__dirname, 'src/components/tailus-ui'),
@@ -22,6 +13,5 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
     },
-    preserveSymlinks: true,
   },
 });
